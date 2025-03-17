48258810c61ca8861704562c95840028cb2c21d4
30lima <pedrolimaa354@gmail.com>
2025-03-17 01:14:39 -0300

    feat: Adding code in class.

--- WealthManagerLib/GerenciadorFinanceiro.cs
+++ WealthManagerLib/GerenciadorFinanceiro.cs
@@ -1,24 +1,24 @@
-using System.Collections.Generic;  // Importa a biblioteca para usar List<T>
+using System.Collections.Generic;
 
 namespace WealthManagerLib
 {
     public class GerenciadorFinanceiro
     {
-        private List<Transacao> transacoes = new List<Transacao>();  // Lista para armazenar transações
+        private List<Transacao> transacoes = new List<Transacao>();
 
-        // Método para adicionar uma transação (receita ou despesa)
+
         public void AdicionarTransacao(Transacao transacao)
         {
             transacoes.Add(transacao);
         }
 
-        // Método para exibir todas as transações cadastradas
+
         public void ExibirTransacoes()
         {
             Console.WriteLine("\n=== Histórico de Transações ===");
-            foreach (var transacao in transacoes)  // Percorre a lista de transações
+            foreach (var transacao in transacoes)
             {
-                transacao.ExibirDetalhes();  // Chama o método ExibirDetalhes() de cada transação
+                transacao.ExibirDetalhes();
             }
             Console.WriteLine("==============================\n");
         }
