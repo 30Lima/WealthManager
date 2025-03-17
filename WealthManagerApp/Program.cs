f9f2d03a558f9fac61401cb9249bfb85e6481e8f
30lima <pedrolimaa354@gmail.com>
2025-03-17 01:19:04 -0300

    feat: Testing the code.

--- WealthManagerApp/Program.cs
+++ WealthManagerApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using WealthManagerLib;
+
+class Program
+{
+    static void Main()
+    {
+        GerenciadorFinanceiro gerenciador = new GerenciadorFinanceiro();
+
+        //Abaixo, estou simulando alguns dados para o projeto
+        gerenciador.AdicionarTransacao(new Receita("Salário", 3000, DateTime.Now));
+        gerenciador.AdicionarTransacao(new Despesa("Aluguel", 1200, DateTime.Now));
+        gerenciador.AdicionarTransacao(new Despesa("Supermercado", 500, DateTime.Now));
+
+        // Exibindo as transações no terminal 
+        gerenciador.ExibirTransacoes();
+    }
+}
